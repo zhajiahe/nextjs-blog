a693c46d0659b7427b9547e28f635846d3de8a1c
zhajiahe <49464122+zhajiahe@users.noreply.github.com>
2025-10-19 10:15:47 +0800

    增加一个project项目（image bed）

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,9 +10,17 @@ const projectsData: Project[] = [
     title: '宝宝成长记录网站',
     description: `一个记录宝宝成长过程的网站，包括宝宝的照片、成长记录、日记里程碑等。
     使用Cursor Vibe Coding完成，支持本地部署和Vercel部署（需要有域名）`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: 'https://image.20250530.space/2025/10/1760840104864-pasted-1760840100405.png',
     href: 'https://github.com/zhajiahe/my-baby',
   },
+  {
+    title: '个人web图床',
+    description: `基于cloudflare免费资源实现的个人免费图床。
+    同样使用Cursor Vibe Coding完成，支持Cloudflare Worker部署，
+    主要用于个人博客内的图片展示`,
+    imgSrc: 'https://image.20250530.space/2025/10/1760596214950-pasted-1760596217728.png',
+    href: 'https://github.com/zhajiahe/r2-image-host'
+  }
 ]
 
 export default projectsData
