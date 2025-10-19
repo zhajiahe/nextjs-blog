04d081a143dd3e00dff569208bd206aaa05fec9f
zhanghuaao <zhajiahe1994@gmail.com>
2025-10-19 10:21:10 +0800

    修复确保了外部图片链接不会被错误地修改，同时也满足了 Next.js 的安全配置要求

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,12 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  // Only prepend basePath for relative paths, not external URLs
+  const isExternalUrl = src?.startsWith('http://') || src?.startsWith('https://')
+  const finalSrc = isExternalUrl ? src : `${basePath || ''}${src}`
+
+  return <NextImage src={finalSrc} {...rest} />
+}
 
 export default Image
--- next.config.js
+++ next.config.js
@@ -78,6 +78,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'image.20250530.space',
+        },
       ],
       unoptimized,
     },
